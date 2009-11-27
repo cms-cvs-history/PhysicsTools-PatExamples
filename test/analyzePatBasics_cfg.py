b748bd53bf542266b7044ab225bb2de655a8272b
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-11-27 16:01:53 +0000

    added more comments to the cfg files

--- test/analyzePatBasics_cfg.py
+++ test/analyzePatBasics_cfg.py
@@ -1,15 +1,24 @@
 import FWCore.ParameterSet.Config as cms
 
+## define process with name 'Test'
 process = cms.Process("Test")
 
+## define input source
 process.source = cms.Source("PoolSource",
   fileNames = cms.untracked.vstring(
     'file:PATLayer1_Output.fromAOD_full.root'
   )
 )
 
+## define MessageLoggerService
 process.MessageLogger = cms.Service("MessageLogger")
 
+## define TFileService for histogramming
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string('analyzePatBasics.root')
+                                   )
+
+## define EDAnalyzer module
 process.analyzeBasicPat = cms.EDFilter("PatBasicAnalyzer",
   photonSrc   = cms.untracked.InputTag("cleanLayer1Photons"),
   electronSrc = cms.untracked.InputTag("cleanLayer1Electrons"),
@@ -19,9 +28,6 @@ process.analyzeBasicPat = cms.EDFilter("PatBasicAnalyzer",
   metSrc      = cms.untracked.InputTag("layer1METs")
 )
 
-process.TFileService = cms.Service("TFileService",
-                                   fileName = cms.string('analyzePatBasics.root')
-                                   )
-
+## process path
 process.p = cms.Path(process.analyzeBasicPat)
 
--- test/analyzePatBasics_modified_cfg.py
+++ test/analyzePatBasics_modified_cfg.py
@@ -1,14 +1,22 @@
 import FWCore.ParameterSet.Config as cms
 
+## define process with name 'Test'
 process = cms.Process("Test")
 
+## define input source
 process.source = cms.Source("PoolSource",
   fileNames = cms.untracked.vstring(
     'file:PATLayer1_Output.fromAOD_full.root'
   )
 )
 
+## define MessageLoggerService
 process.MessageLogger = cms.Service("MessageLogger")
+
+## define TFileService for histogramming
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string('analyzePatBasics.root')
+                                   )
 
 ## using the process.load function
 #process.load("PhysicsTools/PatExamples/PatBasicAnalyzer_cfi")
@@ -20,9 +28,6 @@ process.analyzeBasicPat = analyzeBasicPat
 ## cloning an existing module
 process.analyzeBasicPat2= analyzeBasicPat.clone()
 
-process.TFileService = cms.Service("TFileService",
-                                   fileName = cms.string('analyzePatBasics.root')
-                                   )
-
+## process path
 process.p = cms.Path(process.analyzeBasicPat * process.analyzeBasicPat2)
 
