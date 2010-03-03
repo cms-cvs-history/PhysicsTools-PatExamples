813ada90f50859a8b5ca939aec5a4883bceeb44e
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-03-03 00:24:08 +0000

    first commit for Exercise 1 for the PAT Tutorial March 2010

--- test/patTuple_config_cfg.py
+++ test/patTuple_config_cfg.py
@@ -32,6 +32,6 @@ process.p = cms.Path(
 #     '/store/relval/CMSSW_3_1_1/RelValCosmics/GEN-SIM-RECO/STARTUP31X_V1-v2/0002/7625DA7D-E36B-DE11-865A-000423D174FE.root'
 #                               ]         ##  (e.g. 'file:AOD.root')
 #   process.maxEvents.input = ...         ##  (e.g. -1 to run on all events)
-#   process.out.outputCommands = [ ... ]  ##  (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
+#   process.out.outputCommands+= [ 'keep *_ak5CaloJets_*_*' ]  ##  (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
 #   process.out.fileName = ...            ##  (e.g. 'myTuple.root')
 process.options.wantSummary = True        ##  (to suppress the long output at the end of the job)    
