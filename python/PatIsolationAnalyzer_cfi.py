926963623994822304b58cafea63c78a1a1368a0
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-03-05 23:43:48 +0000

    cleanup from files which are meant for Exercise 2

--- python/PatIsolationAnalyzer_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-analyzePatIsolation = cms.EDAnalyzer("PatIsolationAnalyzer",
-  ## input for pat Muons
-  muons = cms.InputTag("cleanPatMuons"),
-)                               
