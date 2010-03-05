96b01e52c79d41456cb7e4d1e5d16fb0cad89a6f
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-03-05 23:41:14 +0000

    cleanup from modules, which are meant for Exercise 3

--- python/PatMETAnalyzer_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-analyzePatMETs = cms.EDAnalyzer("PatMETAnalyzer",
-  ## input for pat MET
-  mets = cms.InputTag("patMETs"),
-  ## input for alternative MET collection
-  comp = cms.InputTag("patMETsPF")                        
-)                               
