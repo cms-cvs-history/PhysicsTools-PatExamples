baaec47401f77b41c7973622978d2ea1edf9d717
cmsbuild <>
2010-03-06 14:06:16 +0000

    This commit was manufactured by cvs2git to create branch 'PhysicsTools- PatExamples-patTutorial_mar10_Module1'.
    Sprout from master 2010-03-06 14:06:15 UTC Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch> 'update to 35X and cleanup of obsolete files'
    Cherrypick from PhysicsTools-PatExamples-patTutorial_dec09 2009-11-27 16:02:25 UTC Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch> 'final examples for embedding of event information for upcoming tutorial':
        test/patTuple_embedding_cfg.py
    Delete:
        bin/JPsiFWLiteAnalyzer.cc
        bin/PatMcMatchFWLiteAnalyzer.cc
        bin/testSelection_wplusjets.C
        doc/PatTauAnalyzer_README.txt
        doc/html/index.html
        doc/html/overview.html
        interface/WPlusJetsEventSelector.h
        plugins/MuonAnalyzer.cc
        plugins/MuonAnalyzer.h
        plugins/PatBJetTagAnalyzer.cc
        plugins/PatBJetTrackAnalyzer.cc
        plugins/PatBJetVertexAnalyzer.cc
        plugins/PatBTagAnalyzer.cc
        plugins/PatElectronAnalyzer.cc
        plugins/PatJPsiProducer.cc
        plugins/PatJetAnalyzer.cc
        plugins/PatTauAnalyzer.cc
        plugins/PatTauAnalyzer.h
        plugins/PatTrackAnalyzer.cc
        plugins/PatTriggerAnalyzer.cc
        plugins/PatTriggerAnalyzer.h
        plugins/PatVertexAnalyzer.cc
        plugins/PatZjetsElectronAnalyzer.cc
        plugins/PatZjetsJetAnalyzer.cc
        plugins/ZMuMuTutorialAnalyzer.cc
        python/PatElectronAnalyzer_cfi.py
        python/PatJetAnalyzer_cff.py
        python/PatJetAnalyzer_cfi.py
        python/mcMatch_cfi.py
        python/patJPsiProducer_cfi.py
        python/tagAndProbeAnalysis_cff.py
        src/WPlusJetsEventSelector.cc
        test/MuonAnalyzer_cfg.py
        test/analyzePatBJetTags_cfg.py
        test/analyzePatBJetTracks_cfg.py
        test/analyzePatBJetVertex_cfg.py
        test/analyzePatBTag_cfg.py
        test/analyzePatElectron_cfg.py
        test/analyzePatJets_cfg.py
        test/analyzePatTau_fromAOD_cfg.py
        test/analyzePatTau_fromPatTuple_cfg.py
        test/analyzePatTracks_cfg.py
        test/analyzePatTrigger_cfg.py
        test/analyzePatVertex_cfg.py
        test/analyzeZjetsElectrons_cfg.py
        test/analyzeZjetsJets_cfg.py
        test/checkHlt_cfg.py
        test/patBJetTags_efficiencies.C
        test/patBJetTracks_efficiencies.C
        test/patBJetTracks_showPlots.C
        test/patBJetVertex_efficiencies.C
        test/patBJetVertex_showPlots.C
        test/patElectron_eidEfficiency.C
        test/patElectron_recoEfficiency.C
        test/patEventContent_fromAOD_full_a_cfg.py
        test/patEventContent_fromAOD_full_cfg.py
        test/patTau_idEfficiency.C
        test/patTracks_showPlots.C
        test/produceJPsi_cfg.py
        test/producePatMcMatch_cfg.py
        test/producePatTrigger_cfg.py
        test/vplusjets_cfg.py
        test/zMuMuTutorialAnalysis.py

--- test/patTuple_embedding_cfg.py
+++ test/patTuple_embedding_cfg.py
@@ -3,35 +3,51 @@
 # Starting with a skeleton process which gets imported with the following line
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
-# note that you can use a bunch of core tools of PAT 
-# to taylor your PAT configuration; for a few examples
-# uncomment the following lines
-
-from PhysicsTools.PatAlgos.tools.coreTools import *
-#restrictInputToAOD(process)
-#removeMCMatching(process, 'Muons')
-#removeAllPATObjectsBut(process, ['Muons'])
-#removeSpecificPATObjects(process, ['Electrons', 'Muons', 'Taus'])
-
-#switch off new tau features introduced in 33X to restore 31X defaults
-# new feaures: - shrinkingConeTaus instead of fixedCone ones
-#              - TaNC discriminants attached for shrinkingConeTaus
-#              - default preselection on cleaningLayer1
+# ----------------------------------------------------
+# switch off new tau features introduced in 33X to
+# restore 31X defaults new feaures:
+# - shrinkingConeTaus instead of fixedCone ones
+# - TaNC discriminants attached for shrinkingConeTaus
+# - default preselection on cleaningLayer1
+# ----------------------------------------------------
 from PhysicsTools.PatAlgos.tools.tauTools import *
 switchTo31Xdefaults(process)
+
+# ----------------------------------------------------
+# EXERCISE 1: embedding of calo towers in jets
+# ----------------------------------------------------
+## add calo towers to the event content
+#process.out.outputCommands += ["keep *_towerMaker_*_*"]
+
+## drop calo towers but embed those, which belong to
+## jets
+#process.allLayer1Jets.embedCaloTowers = True
+
+
+# ----------------------------------------------------
+# EXERCISE 2: embedding of tracks in electrons
+# ----------------------------------------------------
+## add general tracks to the event content (have fun with it trying w/o embedding ;-))
+#process.out.outputCommands += ["keep *_electronGsfTracks_*_*", "keep *_gsfElectron*_*_*"]
+
+## drop general tracks but embed the track, which belongs
+## to the electrons
+#process.allLayer1Electrons.embedGsfTrack = False
 
 # let it run
 process.p = cms.Path(
     process.patDefaultSequence
     )
 
-# In addition you usually want to change the following parameters:
-#
-#   process.GlobalTag.globaltag =  ...    ##  (according to https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions)
-#   process.source.fileNames = [
-#     '/store/relval/CMSSW_3_1_1/RelValCosmics/GEN-SIM-RECO/STARTUP31X_V1-v2/0002/7625DA7D-E36B-DE11-865A-000423D174FE.root'
-#                               ]         ##  (e.g. 'file:AOD.root')
-#   process.maxEvents.input = ...         ##  (e.g. -1 to run on all events)
-#   process.out.outputCommands = [ ... ]  ##  (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
-#   process.out.fileName = ...            ##  (e.g. 'myTuple.root')
-process.options.wantSummary = True        ##  (to suppress the long output at the end of the job)    
+# ----------------------------------------------------
+# You might want to change some of these default
+# parameters
+# ----------------------------------------------------
+#process.GlobalTag.globaltag =  ...    ##  (according to https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions)
+#process.source.fileNames = [
+#'/store/relval/CMSSW_3_1_1/RelValCosmics/GEN-SIM-RECO/STARTUP31X_V1-v2/0002/7625DA7D-E36B-DE11-865A-000423D174FE.root'
+#                            ]         ##  (e.g. 'file:AOD.root')
+#process.maxEvents.input = ...         ##  (e.g. -1 to run on all events)
+#process.out.outputCommands = [ ... ]  ##  (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
+#process.out.fileName = ...            ##  (e.g. 'myTuple.root')
+#process.options.wantSummary = True    ##  (to suppress the long output at the end of the job)
