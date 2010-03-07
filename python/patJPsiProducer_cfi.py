5082268f7c7950b9eef764a60efeaa52ae4944b9
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-03-07 16:37:56 +0000

    accidental tag

--- python/patJPsiProducer_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-patJPsiCandidates = cms.EDFilter("PatJPsiProducer",
-                                 muonSrc = cms.InputTag("selectedPatMuons")
-                                 )
