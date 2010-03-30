fc0e1e21c9b8d32a29cfb62fc6e6c36686e065f0
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2010-03-30 19:45:21 +0000

    Run over run number 132440

--- test/patLayer1_fromRECO_7TeV_firstdata_cfg.py
+++ test/patLayer1_fromRECO_7TeV_firstdata_cfg.py
@@ -6,7 +6,7 @@ from PhysicsTools.PatAlgos.patTemplate_cfg import *
 from PhysicsTools.PatAlgos.tools.coreTools import *
 
 ## global tag for data
-process.GlobalTag.globaltag = cms.string('START3X_V25::All')
+process.GlobalTag.globaltag = cms.string('GR10_P_V2::All')
 
 # turn off MC matching for the process
 #removeMCMatching(process, ['All'])
@@ -80,24 +80,41 @@ readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring()
 
 readFiles.extend( [
-       'file:0CBD356D-E53B-DF11-AC6F-000423D9970C.root',
-       'file:6E907338-E83B-DF11-9DB1-000423D98A44.root'
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/F4C92A98-163C-DF11-9788-0030487C7392.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/F427D642-173C-DF11-A909-0030487C60AE.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/E27821C3-0C3C-DF11-9BD9-0030487CD718.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/D87D5469-2E3C-DF11-A470-000423D99896.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/B647CAD9-0E3C-DF11-886F-0030487CD716.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/A860D55E-193C-DF11-BE29-0030487C60AE.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/9884BC11-0C3C-DF11-8F9C-000423D986C4.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/92684831-233C-DF11-ABA0-0030487CD16E.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/90269E76-0D3C-DF11-A1A0-0030487CD840.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/8CAE3014-133C-DF11-A05D-000423D174FE.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/8C51BAC6-1A3C-DF11-A0EE-000423D94A04.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/8C042B04-2D3C-DF11-939F-0030487CD178.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/80471A6B-0E3C-DF11-8DCD-0030487C6A66.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/762824C3-0C3C-DF11-A4FD-0030487CD6D2.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/6A3533F5-103C-DF11-B3AA-00304879BAB2.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/4C8979D2-073C-DF11-B97B-000423D6AF24.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/26C8DED9-0E3C-DF11-9D83-0030487CD7B4.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/181C44F7-093C-DF11-A9CB-001D09F24FEC.root',
+'/store/data/Commissioning10/MinimumBias/RECO/v7/000/132/440/0AA7C390-0F3C-DF11-BD65-000423D998BA.root'
         ] );
 process.source.fileNames = readFiles
 
 # let it run
 
-print
-print "============== Warning =============="
-print "technical trigger filter:    DISABLED"
-print "physics declare bit filter:  DISABLED"
-print "primary vertex filter:       DISABLED"
+#print
+#print "============== Warning =============="
+#print "technical trigger filter:    DISABLED"
+#print "physics declare bit filter:  DISABLED"
+#print "primary vertex filter:       DISABLED"
 
 process.p = cms.Path(
-#    process.hltLevel1GTSeed*
+    process.hltLevel1GTSeed*
     process.scrapingVeto*
-#    process.physDecl*
-#    process.primaryVertexFilter*
+    process.physDecl*
+    process.primaryVertexFilter*
     process.patDefaultSequence
     )
 
