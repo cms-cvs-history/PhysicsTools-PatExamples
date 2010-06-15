f19a5e40fc7a6f7ed2fea33d907272fdbfcc36cb
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-06-15 22:22:19 +0000

    added some preselection typical for ttbar events

--- test/producePatTrigger_cfg.py
+++ test/producePatTrigger_cfg.py
@@ -10,9 +10,15 @@ process.p = cms.Path(
     process.patDefaultSequence
 )
 
-process.maxEvents.input     = 1000 # Reduce number of events for testing.
+process.maxEvents.input     = -1 # Reduce number of events for testing.
 process.out.fileName        = 'edmPatTrigger.root'
 process.options.wantSummary = False # to suppress the long output at the end of the job
+
+process.selectedPatMuons.cut ='isTrackerMuon=1 & isGlobalMuon()=1'
+                              'innerTrack.numberOfValidHits>=11 &'
+                              'globalTrack.normalizedChi2<10.0  &'
+                              'globalTrack.hitPattern.numberOfValidMuonHits>0 &'
+                              'abs(dB)<0.02 & (trackIso+caloIso)/pt<0.05'
 
 # PAT trigger
 process.load( "PhysicsTools.PatAlgos.triggerLayer1.triggerProducer_cff" )
