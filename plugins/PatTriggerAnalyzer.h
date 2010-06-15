d69c166f79faeff7e82c054306bf6a60fb550f64
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-06-15 22:04:07 +0000

    added a few more comments

--- plugins/PatTriggerAnalyzer.h
+++ plugins/PatTriggerAnalyzer.h
@@ -1,52 +1,57 @@
 #ifndef PhysicsTools_PatExamples_PatTriggerAnalyzer_h
 #define PhysicsTools_PatExamples_PatTriggerAnalyzer_h
 
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
-
-#include <string>
 #include <map>
-
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/Utilities/interface/InputTag.h"
-
-#include "DataFormats/PatCandidates/interface/TriggerEvent.h"
-#include "DataFormats/PatCandidates/interface/Muon.h"
+#include <string>
 
 #include "TH1D.h"
 #include "TH2D.h"
 
-namespace pat {
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/TriggerEvent.h"
 
-  class PatTriggerAnalyzer : public edm::EDAnalyzer {
 
-    public:
+class PatTriggerAnalyzer : public edm::EDAnalyzer {
+  
+ public:
+  /// default constructor  
+  explicit PatTriggerAnalyzer( const edm::ParameterSet & iConfig );
+  /// default destructor
+  ~PatTriggerAnalyzer();
+  
+ private:
+  /// everythin that needs to be done before the event loop
+  virtual void beginJob();
+  /// everythin that needs to be done during the event loop
+  virtual void analyze( const edm::Event & iEvent, const edm::EventSetup & iSetup );
+  /// everythin that needs to be done after the event loop
+  virtual void endJob();
 
-      explicit PatTriggerAnalyzer( const edm::ParameterSet & iConfig );
-      ~PatTriggerAnalyzer();
-
-    private:
-
-      virtual void beginJob() ;
-      virtual void analyze( const edm::Event & iEvent, const edm::EventSetup & iSetup );
-      virtual void endJob();
-
-      edm::InputTag trigger_;
-      edm::InputTag triggerEvent_;
-      edm::InputTag muons_;
-      std::string   muonMatch_;
-
-      unsigned minID_;
-      unsigned maxID_;
-
-      std::map< std::string, TH1D* > histos1D_;
-      std::map< std::string, TH2D* > histos2D_;
-
-      std::map< unsigned, unsigned > sumN_;
-      std::map< unsigned, double >   sumPt_;
-  };
-
-}
+  /// input for patTrigger  
+  edm::InputTag trigger_;
+  /// input for patTriggerEvent
+  edm::InputTag triggerEvent_;
+  /// input for muons
+  edm::InputTag muons_;
+  /// input for trigger match objects
+  std::string   muonMatch_;
+  /// minimal id for meanPt plot
+  unsigned minID_;
+  /// maximal id for meanPt plot
+  unsigned maxID_;
+  
+  /// histogram management
+  std::map< std::string, TH1D* > histos1D_;
+  std::map< std::string, TH2D* > histos2D_;
+  
+  /// internals for meanPt histogram calculation
+  std::map< unsigned, unsigned > sumN_;
+  std::map< unsigned, double >   sumPt_;
+};
 
 #endif
