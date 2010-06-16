5fd6c3fdacbd91e020fe2fc72bb121b885a8dcb9
cmsbuild <>
2010-06-16 15:57:37 +0000

    This commit was manufactured by cvs2git to create tag 'PhysicsTools- PatExamples-patTutorial_June10_Exercise7'.
    Sprout from PhysicsTools-PatExamples-patTutorial_mar10_Module1 2010-06-16 15:57:36 UTC Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch> 'absorbed fwlite config into basic analyzer example'
    Delete:
        BuildFile.xml
        bin/PatBasicFWLiteAnalyzer.cc
        bin/analyzePatFWLite_cfg.py
        interface/BTagPerformance.h
        interface/PatBTagCommonHistos.h
        plugins/BuildFile.xml
        plugins/PatBasicAnalyzer.cc
        python/PatBasicAnalyzer_cfi.py
        src/PatBTagCommonHistos.cc
        test/analyzePatBasics_cfg.py
        test/analyzePatBasics_modified_cfg.py
        test/patTuple_inputs_cfg.py
        test/patTuple_standard_cfg.py

--- bin/analyzePatFWLite_cfg.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-process = cms.Process("FWLitePlots")
-
-process.FWLiteParams = cms.PSet(
-  input   = cms.string('file:patTuple.root'),
-  muonSrc = cms.InputTag('cleanPatMuons')
-)
-
