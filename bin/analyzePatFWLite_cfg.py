57ee63b38587861f040f34e020e3d200e935538b
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2010-12-29 21:29:17 +0000

    Updating to new FWLite I/O infrastructure

--- bin/analyzePatFWLite_cfg.py
+++ bin/analyzePatFWLite_cfg.py
@@ -2,13 +2,17 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("FWLitePlots")
 
+process.fwliteInput = cms.PSet(
+    fileNames   = cms.vstring(['file:patTuple.root']),  ## mandatory
+    maxEvents   = cms.int32(-1),
+    outputEvery = cms.uint32(10)
+    )
+
+process.fwliteOutput = cms.PSet(
+    fileName  = cms.string('analyzePatBasics.root') ## mandatory
+    )
+
 process.MuonAnalyzer = cms.PSet(
-    ## common input for wrapped analyzers
-    fileNames   = cms.vstring('file:patTuple.root'),  ## mandatory
-    outputFile  = cms.string('analyzePatBasics.root'),## mandatory
-    maxEvents   = cms.int32(-1),                      ## optional
-    outputEvery = cms.uint32(10),                     ## optional
-    ## input specific for this analyzer
     muons = cms.InputTag('cleanPatMuons')
 )
 
